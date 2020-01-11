ff8fe307ee5e17b85473a25371b565187ca78f72
Ketomaa <tketomaa5@gmail.com>
2020-01-11 13:01:03 +0200

    Done

---
+++ StringTasks/StringTask1/StringTask1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Ohjelma laskee merkkien määrän.");
+                Console.Write("Syötä teksti: ");
+                string text = Console.ReadLine();
+                Console.WriteLine(text.Length);
+            }
+        }
+    }
+}
